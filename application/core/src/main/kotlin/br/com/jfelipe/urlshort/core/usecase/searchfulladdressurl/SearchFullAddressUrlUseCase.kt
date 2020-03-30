c5b325034d3ec772c77b6046c0c8d7cb5723d204
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2020-03-30 19:43:07 -0300

    Removendo ponto da frase

--- application/core/src/main/kotlin/br/com/jfelipe/urlshort/core/usecase/searchfulladdressurl/SearchFullAddressUrlUseCase.kt
+++ application/core/src/main/kotlin/br/com/jfelipe/urlshort/core/usecase/searchfulladdressurl/SearchFullAddressUrlUseCase.kt
@@ -3,5 +3,5 @@ package br.com.jfelipe.urlshort.core.usecase.searchfulladdressurl
 class SearchFullAddressUrlUseCase(private val getUrl: GetUrl) {
 
     fun searchFullAddressUrl(shortAddressUrl: String) = getUrl.getFullAddressUrl(shortAddressUrl)
-        ?: throw UrlNotFoundException("Short URL $shortAddressUrl not found.")
+        ?: throw UrlNotFoundException("Short URL $shortAddressUrl not found")
 }
