d5bc1a8dca8339821b872991ecf841aebcea3887
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2020-04-09 12:24:01 -0300

    Add pre-requisitos para criacao do objeto

--- application/core/src/main/kotlin/br/com/jfelipe/urlshort/core/entity/Url.kt
+++ application/core/src/main/kotlin/br/com/jfelipe/urlshort/core/entity/Url.kt
@@ -1,3 +1,17 @@
 package br.com.jfelipe.urlshort.core.entity
 
-data class Url(val shortAddress: String, val fullAddress: String)
+import org.apache.commons.codec.binary.Base64.isBase64
+import org.apache.commons.validator.routines.UrlValidator
+
+data class Url private constructor(val shortAddress: String, val fullAddress: String) {
+    companion object {
+        operator fun invoke(shortAddress: String, fullAddress: String): Url {
+            val urlValidator = UrlValidator(arrayOf("http", "https"))
+
+            require(isBase64(shortAddress)) { "Short url is not valid" }
+            require(urlValidator.isValid(fullAddress)) { "Url is not valid" }
+
+            return Url(shortAddress, fullAddress)
+        }
+    }
+}
