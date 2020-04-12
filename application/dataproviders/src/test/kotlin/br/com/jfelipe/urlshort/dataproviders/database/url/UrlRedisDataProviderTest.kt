fb939feebec4c78b2fe694f60104988fec93f1c5
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2020-04-12 11:47:47 -0300

    Alterando nome do pacote

--- application/dataproviders/src/test/kotlin/br/com/jfelipe/urlshort/dataproviders/database/url/UrlRedisDataProviderTest.kt
+++ application/dataproviders/src/test/kotlin/br/com/jfelipe/urlshort/dataproviders/database/url/UrlRedisDataProviderTest.kt
@@ -1,4 +1,4 @@
-package br.com.jfelipe.urlshort.dataproviders.url
+package br.com.jfelipe.urlshort.dataproviders.database.url
 
 import br.com.jfelipe.urlshort.core.entity.Url
 import com.nhaarman.mockito_kotlin.any
