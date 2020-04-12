c2ed4646251cf83246301f3f888e930b0402c83e
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2020-04-12 11:32:43 -0300

    Add testes de integracao para base de dados

---
+++ integration-tests/src/test/kotlin/br/com/jfelipe/urlshort/integration/database/url/UrlRedisDataProviderIntegrationTest.kt
@@ -0,0 +1,32 @@
+package br.com.jfelipe.urlshort.integration.database.url
+
+import br.com.jfelipe.urlshort.core.entity.Url
+import br.com.jfelipe.urlshort.dataproviders.database.url.UrlRedisDataProvider
+import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import javax.inject.Inject
+
+@MicronautTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class UrlRedisDataProviderIntegrationTest {
+
+    @Inject
+    private lateinit var database: UrlRedisDataProvider
+
+    @Test
+    @Order(1)
+    fun `Should save a new url in database`() {
+        database.insertUrl(Url("loggi", "https://loggi.com"))
+    }
+
+    @Test
+    @Order(2)
+    fun `Should return full address url`() {
+        val url = database.getFullAddressUrl("loggi")
+        assertNotNull(url)
+    }
+}
