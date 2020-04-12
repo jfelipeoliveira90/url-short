4294edabeae12c4aaa47c5f553c985def644b9d8
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2020-04-12 11:41:56 -0300

    Add valor padrao para atributo shortAddress

--- application/entrypoints/src/main/kotlin/br/com/jfelipe/urlshort/entrypoints/rest/url/UrlDTO.kt
+++ application/entrypoints/src/main/kotlin/br/com/jfelipe/urlshort/entrypoints/rest/url/UrlDTO.kt
@@ -3,4 +3,4 @@ package br.com.jfelipe.urlshort.entrypoints.rest.url
 import io.micronaut.core.annotation.Introspected
 
 @Introspected
-data class UrlDTO(val fullAddress: String, val shortAddress: String?)
+data class UrlDTO(val fullAddress: String, val shortAddress: String? = null)
