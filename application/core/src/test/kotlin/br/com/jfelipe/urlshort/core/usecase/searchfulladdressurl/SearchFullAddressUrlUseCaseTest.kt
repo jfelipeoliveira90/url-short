ccb8c7c50654e16212bbd8ae2ad3760c90574192
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2020-04-14 09:27:16 -0300

    Organizando testes para aula

--- application/core/src/test/kotlin/br/com/jfelipe/urlshort/core/usecase/searchfulladdressurl/SearchFullAddressUrlUseCaseTest.kt
+++ application/core/src/test/kotlin/br/com/jfelipe/urlshort/core/usecase/searchfulladdressurl/SearchFullAddressUrlUseCaseTest.kt
@@ -1,42 +1,5 @@
 package br.com.jfelipe.urlshort.core.usecase.searchfulladdressurl
 
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.junit.jupiter.api.assertThrows
-import org.junit.platform.commons.util.StringUtils.isNotBlank
-
-@TestInstance(PER_CLASS)
 class SearchFullAddressUrlUseCaseTest {
 
-    private lateinit var useCase: SearchFullAddressUrlUseCase
-    private lateinit var getUrl: GetUrl
-
-    @BeforeAll
-    fun setup() {
-        getUrl = mock {
-            on { getFullAddressUrl(any()) } doReturn listOf(
-                "https://www.loggi.com/compartilhe/corp/86b35e3a1f2bf7b8f77c7ef30c3cd159",
-                null
-            )
-        }
-
-        useCase = SearchFullAddressUrlUseCase(getUrl)
-    }
-
-    @Test
-    fun `Should return full url address`() {
-        val url = useCase.searchFullAddressUrl("loggi")
-        assertTrue(isNotBlank(url))
-    }
-
-    @Test
-    fun `Should return exception for url not found`() {
-        assertThrows<UrlNotFoundException> { useCase.searchFullAddressUrl("rappi") }
-    }
 }
--- application/dataproviders/src/test/kotlin/br/com/jfelipe/urlshort/dataproviders/database/url/UrlRedisDataProviderTest.kt
+++ application/dataproviders/src/test/kotlin/br/com/jfelipe/urlshort/dataproviders/database/url/UrlRedisDataProviderTest.kt
@@ -1,41 +1,5 @@
 package br.com.jfelipe.urlshort.dataproviders.database.url
 
-import br.com.jfelipe.urlshort.core.entity.Url
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
-import io.lettuce.core.api.StatefulRedisConnection
-import io.lettuce.core.api.sync.RedisCommands
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Test
-
 class UrlRedisDataProviderTest {
 
-    private val shortAddress = "0bkZ2V"
-    private val fullAddress = "https://www.loggi.com/compartilhe/corp/86b35e3a1f2bf7b8f77c7ef30c3cd159"
-
-    private val syncCommands = mock<RedisCommands<String, String>> {
-        on { set(any(), any()) } doReturn shortAddress
-        on { get(any()) } doReturn fullAddress
-    }
-
-    private val connection = mock<StatefulRedisConnection<String, String>> {
-        on { sync() } doReturn syncCommands
-    }
-
-    private val dataProvider = UrlRedisDataProvider(connection)
-
-    @Test
-    fun `Should save a new url in database`() {
-        dataProvider.insertUrl(Url(shortAddress, fullAddress))
-        verify(syncCommands, times(1)).set(any(), any())
-    }
-
-    @Test
-    fun `Should return full address url`() {
-        val url = dataProvider.getFullAddressUrl(shortAddress)
-        assertNotNull(url)
-    }
 }
